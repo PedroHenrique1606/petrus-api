6c1acce13ca611355758910dbf0f7948ee7d241b
PedroHenrique1606 <pedromelo.dev.contato@gmail.com>
2025-04-04 10:14:18 -0300

    fix(auth controller refactored from login dto): :zap:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('login')
-    async login(@Body() body: { email: string; password: string }) {
+    @ApiOperation({ summary: 'Login do usuário' })
+    @ApiBody({ type: LoginDto })
+    async login(@Body() body: LoginDto) {
         return this.authService.loginWithCredentials(body.email, body.password);
     }
 }
